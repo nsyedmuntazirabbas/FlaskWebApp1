d9952ad9956cd2523c374b17610cbd039778df57
Syed Muntazir Abbas naqvi <nsyedmuntazirabbas@gmail.com>
2025-12-04 23:01:15 +0100

    Update runserver.py

--- flask_devops_app/runserver.py
+++ flask_devops_app/runserver.py
@@ -10,9 +10,4 @@ from FlaskWebProject1 import app
 load_dotenv()
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', '0.0.0.0')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '8000'))
-    except ValueError:
-        PORT = 5555
-    app.run(host=HOST, port=PORT)
+      app.run(host="0.0.0.0", port=8000)
